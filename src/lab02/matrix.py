c31ac304102a34f257aa5208d366c314632eb9c3
Damix222 <spiridonovaroslav004@gmail.com>
2025-10-09 10:57:10 +0300

    .

---
+++ src/lab02/matrix.py
@@ -0,0 +1,44 @@
+print('Вывод на задание transpose:')
+def transpose(mat: list[list[float | int]]) -> list[list]:
+    if len(mat) == 0:
+        return []
+    row_1len = len(mat[0])
+    for row in mat:
+        if len(row) != row_1len:
+            return 'ValueError'
+    return [list(pillar) for pillar in zip(*mat)]
+print(transpose([[1, 2, 3]]))
+print(transpose([[1], [2], [3]]))
+print(transpose([[1, 2], [3, 4]]))
+print(transpose([]))
+print(transpose([[1, 2], [3]]))
+        
+print('Вывод на задание row_sums:')
+def row_sums(mat: list[list[float | int]]) -> list[float]:
+    list_length = (len(row) for row in mat)
+    if len(set(list_length)) != 1:
+        return 'ValueError'
+    result = 0
+    return list(map(int, (sum(x) for x in mat)))
+print(row_sums([[1, 2, 3], [4, 5, 6]]))
+print(row_sums([[-1, 1], [10, -10]]))
+print(row_sums([[0, 0], [0, 0]]))
+print(row_sums([[1, 2], [3]]))
+
+print('Вывод на задание col_sums:')
+def col_sums(mat: list[list[float | int]]) -> list[float]:
+    list_length = (len(row) for row in mat)
+    if len(set(list_length)) != 1:
+        return 'ValueError'
+    summa_mat = []
+    for j in range(len(mat[0])):
+        summa = 0
+        for i in range(len(mat)):
+            summa += mat[i][j]
+        summa_mat.append(summa)
+    return(summa_mat)
+print(col_sums([[1, 2, 3], [4, 5, 6]]))
+print(col_sums([[-1, 1], [10, -10]]))
+print(col_sums([[0, 0], [0, 0]]))
+print(col_sums([[1, 2], [3]]))
+            
---
+++ src/lab02/tuples.py
@@ -0,0 +1,24 @@
+def format_record(rec: tuple[str, str, float]) -> str:
+    if len(rec) != 3 or not(isinstance(rec, tuple)):
+        return 'TypeError'
+    fio, group, gpa = rec
+    if len(fio) == 0 or len(group) == 0:
+        return 'TypeError'#неверная длина
+    if not isinstance(gpa, (int, float)) or not (0<gpa<=5):
+        return 'ValueErrror'#неверно введен тип данных
+    new_rec = ''
+    fio = fio.strip().split()
+    if len(fio) == 2:
+        fio = str(fio[0][0].upper() + fio[0][1:] + fio[1][0].upper() + '., ')
+        new_rec += fio
+    if len(fio) == 3:
+        fio = str(fio[0][0].upper() + fio[0][1:] + ' ' + fio[1][0].upper() + '.' + fio[2][0].upper() + '., ')
+        new_rec += fio
+    gpa = f'{gpa:.2f}'
+    new_rec += 'гр. ' + str(group) + ', ' + 'GPA ' +  str(gpa)
+    return new_rec
+print(format_record(("Иванов Иван иванович", "BIVT-25", 4.6)))
+print(format_record(("Петров Пётр", "IKBO-12", 5.0)))
+print(format_record(("Петров Пётр Петрович", "IKBO-12", 5.0)))
+print(format_record(("  сидорова  анна   сергеевна ", "ABB-01", 3.999)))
+print(format_record(("  сидорова  анна   сергеевна ", "ABB-01")))
